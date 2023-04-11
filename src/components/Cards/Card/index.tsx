934184e4f959b500a25bcca3bb2b5621112cfef4
Leonardo Valdetaro <leonardovaldetaro@gmail.com>
2023-04-11 09:26:20 +0100

    restaurando a pasta dos cards

--- src/components/Cards/Card/index.tsx
+++ src/components/Cards/Card/index.tsx
@@ -2,34 +2,24 @@ import Styles from './Card.module.scss';
 import CardsInfo from 'data/cardsInfo.json';
 import classNames from 'classnames';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/scss';
-
 type Props = typeof CardsInfo[0];
 
 export default function Card (props: Props) {
     const {photo, category, description, title} = props;
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide className={Styles.card}>
-                <img src={photo} alt="Imagem" className={Styles.card__img} />
+        <article className={Styles.card}>
+            <img src={photo} alt="Imagem" className={Styles.card__img} />
 
-                <h2 className={Styles.card__title}>{title}</h2>
+            <h2 className={Styles.card__title}>{title}</h2>
 
-                <h3 className={classNames({
-                    [Styles.card__tag]: true,
-                    [Styles[`card__tag__${category.label.toLowerCase()}`]]: true,
-                })}
-                >
-                    {category.label}
-                </h3>
-                <p className={Styles.card__descricao}>{description} </p>
-            </SwiperSlide>
-        </Swiper>
+            <h3 className={classNames({
+                [Styles.card__tag]: true,
+                [Styles[`card__tag__${category.label.toLowerCase()}`]]: true,
+            })}
+            >
+                {category.label}
+            </h3>
+            <p className={Styles.card__descricao}>{description} </p>
+        </article>
     );
 }
